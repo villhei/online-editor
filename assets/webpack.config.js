09e357e67494d17974aadd23aa9bcb1369aa738d
Ville Heikkinen <ville.heikkinen@wunderdog.fi>
2017-09-19 10:03:53 +0300

    Use semantic-ui with less

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,12 +2,38 @@ const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const WebpackShellPlugin = require('webpack-shell-plugin')
+const RewriteImportPlugin = require('less-plugin-rewrite-import')
+
+function getPlugins () {
+  const plugins = [
+    new ExtractTextPlugin('css/app.css'),
+    new CopyWebpackPlugin([{ from: './static/' }])
+  ]
+  if (!process.env.SKIP_LINT) {
+    return plugins.concat(new WebpackShellPlugin({ onBuildStart: ['npm run fix-style'], onBuildEnd: ['echo "Webpack End"'] }))
+  }
+  return plugins
+}
+
+const lessLoader = {
+  loader: 'less-loader', // compiles Less to CSS
+  options: {
+    paths: [path.resolve(__dirname), path.resolve(__dirname, 'node_modules')],
+    plugins: [
+      new RewriteImportPlugin({
+        paths: {
+          '../../theme.config': path.join(__dirname, '/css/semantic-ui/theme.config')
+        }
+      })
+    ]
+  }
+}
 
 const config = {
-  entry: ['./css/app.css', './src/app.js'],
+  entry: ['./css/app.less', './src/app.js'],
   output: {
     path: path.resolve(__dirname, '../priv/static'),
-    filename: 'src/app.js'
+    filename: 'js/app.js'
   },
   devtool: 'source-map',
   resolve: {
@@ -25,10 +51,14 @@ const config = {
         use: 'babel-loader'
       },
       {
-        test: /\.css$/,
+        test: /\.less$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
+          use: [{
+            loader: 'css-loader'
+          },
+            lessLoader
+          ]
         })
       },
       {
@@ -38,16 +68,16 @@ const config = {
         'source-map-loader'
       },
       {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$/,
+        use: 'file-loader?name=[name].[ext]?[hash]'
+      },
+      {
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         loader: 'file-loader?name=fonts/[name].[ext]'
       }
     ]
   },
-  plugins: [
-    new WebpackShellPlugin({onBuildStart: ['npm run fix-style'], onBuildEnd: ['echo "Webpack End"']}),
-    new ExtractTextPlugin('css/app.css'),
-    new CopyWebpackPlugin([{ from: './static/' }])
-  ]
+  plugins: getPlugins()
 }
 
 module.exports = config
