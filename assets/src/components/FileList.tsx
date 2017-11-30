b5dda2a1aa0100f62e71d2341901a880504b8d05
Ville Heikkinen <ville.heikkinen@wunderdog.fi>
2017-11-30 09:41:56 +0200

    Sort documents

--- assets/src/components/FileList.tsx
+++ assets/src/components/FileList.tsx
@@ -7,9 +7,23 @@ type Props = {
   documents: RootState['model']['documents']['all'],
   createDocument: () => void
 }
+
+function sortDocuments(documents: Array<TextDocument>, descending = true): Array<TextDocument> {
+  const sorted: Array<TextDocument> = documents.slice(0)
+    .sort((a, b) => {
+      return new Date(a.updated_at).valueOf() - new Date(b.updated_at).valueOf()
+    })
+  if (descending) {
+    sorted.reverse()
+    return sorted
+  } else {
+    return sorted
+  }
+}
 export default class FileList extends React.Component<Props, any> {
   render() {
     const { documents, createDocument } = this.props
+    const sortedDocuments = sortDocuments(documents)
     return (
       <div className='ui twelve wide centered column'>
         <div className='ui four doubling cards'>
@@ -17,7 +31,7 @@ export default class FileList extends React.Component<Props, any> {
             <div className='ui left aligned small header'><i className='plus icon' />Add new</div>
           </a>
           {
-            documents.map(document => (
+            sortedDocuments.map(document => (
               <Link className='ui padded card' key={document.id} to={`/edit/${document.id}`} >
                 <div className='ui left aligned small header'><i className='file icon' />{document.name}</div>
                 <div className='ui divider' />
