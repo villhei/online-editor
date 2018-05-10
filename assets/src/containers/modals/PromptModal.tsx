0e0d01e8bf5a924139154b33b53b633a6cc99593
Ville Heikkinen <ville.heikkinen@wunderdog.fi>
2018-05-10 12:53:13 +0300

    Attempt to submit on enter

--- assets/src/containers/modals/PromptModal.tsx
+++ assets/src/containers/modals/PromptModal.tsx
@@ -14,24 +14,52 @@ export type Props = {
   onCancel: () => void
 }
 
+const ESC_KEY = 27
+const ENTER_KEY = 13
+
 export type State = {
   response: string
 }
 
 class PromptModal extends React.Component<Props> {
+  componentDidMount() {
+    window.addEventListener('keydown', this.dismissListener)
+    window.addEventListener('keydown', this.onConfirmListener)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.dismissListener)
+    window.removeEventListener('keydown', this.onConfirmListener)
+  }
+
+  dismissListener = (event: KeyboardEvent) => {
+    if (event.keyCode === ESC_KEY) {
+      this.props.onCancel()
+    }
+  }
+
+  onConfirmListener = (event: KeyboardEvent) => {
+    console.log('onConfirmListener', this.props)
+    if (event.keyCode === ENTER_KEY) {
+      console.log(this.props)
+      if (this.props.isValid) {
+        this.handleConfirm()
+      }
+    }
+  }
 
   handleChange = (event: React.FormEvent<HTMLInputElement>) => {
     this.props.onChange(event)
   }
 
   handleConfirm = () => {
-    this.props.onConfirm()
+    const { onConfirm } = this.props
+    onConfirm()
   }
 
   render() {
     const modalContainer = (document.getElementById('modal') as HTMLDivElement)
     const { icon, title, message, placeholder, value, isValid, onChange, onCancel } = this.props
-
+    console.log('props', this.props)
     return ReactDOM.createPortal(<PromptModalView
       title={title}
       icon={icon}
